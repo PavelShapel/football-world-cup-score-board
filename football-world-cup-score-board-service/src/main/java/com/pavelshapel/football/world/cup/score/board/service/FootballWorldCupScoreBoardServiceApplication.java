bb7bd71b850fbe443fd879b4677b15b6c3639483
Pavel, Shapel <pavel.shapel@gmail.com>
2025-06-03 14:25:52 +0200

    Add football-world-cup-score-board-service module with application configuration

---
+++ football-world-cup-score-board-service/src/main/java/com/pavelshapel/football/world/cup/score/board/service/FootballWorldCupScoreBoardServiceApplication.java
@@ -0,0 +1,12 @@
+package com.pavelshapel.football.world.cup.score.board.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.pavelshapel.football.world.cup.score.board"})
+public class FootballWorldCupScoreBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballWorldCupScoreBoardServiceApplication.class, args);
+    }
+}
